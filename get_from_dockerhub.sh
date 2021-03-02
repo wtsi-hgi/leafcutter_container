e00a3e36c01e5de4acaa7caa5d154b63d4c92ad3
gn5 <gn5@sanger.ac.uk>
2021-03-02 15:23:21 +0000

    auto-git

--- get_from_dockerhub.sh
+++ get_from_dockerhub.sh
@@ -6,9 +6,11 @@ export SINGULARITY_CACHEDIR=$PWD/cache_dir
 mkdir -p tmp_dir                                                                                                                            
 export TMPDIR=$PWD/tmp_dir                                                                                                                  
                                                                                                                                             
-# see https://github.com/wtsi-hgi/scrna_deconvolution_container                                                                             
-rm -f scrna_deconvolution_latest.sif                                                                                                        
-singularity pull docker://mercury/scrna_deconvolution      
-
-rm -r cache_dir
-rm -r tmp_dir
+# see https://github.com/wtsi-hgi/leafcutter_container                                                                                                     
+rm -f leafcutter.sif                                                                                                                                       
+rm -f leafcutter.img                                                                                                                                       
+singularity pull docker://wtsihgi/leafcutter                                                                                                               
+mv leafcutter.sif leafcutter.img                                                                                                                           
+                                                                                                                                                           
+rm -r cache_dir                                                                                                                                            
+rm -r tmp_dir      
